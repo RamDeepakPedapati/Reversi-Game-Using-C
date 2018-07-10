4efce491f5ea256580bcf4c486da0777d32191c7
RamDeepakPedapati <ramdeepakpedapati@gmail.com>
2018-07-10 20:22:34 +0530

    Update main.cpp

--- C_Reversi_game/main.cpp
+++ C_Reversi_game/main.cpp
@@ -2,9 +2,6 @@
 #include "BoardUtils.h"
 #include "BoardMoves.h"
 
-
-
-
 int main(){
 
 	int rows = 4, cols = 4;
@@ -37,42 +34,7 @@ int main(){
 		playMove(game,inputMove(game));
 		clearAndRedrawScreen(game);
 	}
-
 	printWinner(game);
-
-
-	/*int MoveX[] = { 4, 4, 2, 4, 1, 1, 3, 4, 1, 1, 3 };
-	int MoveY[] = { 2, 3, 4, 1, 4, 3, 4, 4, 2, 1, 1 };
-	int whiteMoveCount[] = { 4, 3, 6, 5, 3, 5, 3, 5, 2, 4, 3, };
-	int blackMoveCount[] = { 1, 3, 1, 3, 6, 5, 8, 7, 11, 10, 12 };
-	int turn[] = { 1, -1, 1, -1, -1, 1, -1, 1, -1, 1, -1 };
-
-	for (int i = 0; i < 11; i++){
-
-		playMove(game, createMove(MoveX[i], MoveY[i]));
-		for (int iter1 = 0; iter1 < game->rows; iter1++)
-		{
-			for (int iter2 = 0; iter2 < game->cols; iter2++)
-			{
-				printf("%c ", game->board[iter1][iter2]);
-			}
-			printf("\n");
-		}
-		printf("wc-%d bc-%d\n", game->whiteCount, game->blackCount);
-	}*/
-
-
-
-
-
-
-
-
-
-
-	
-
-
 	system("pause");
 	return 0;
 }
